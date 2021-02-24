7aab51eff1f5d5d68d4e89716d3ad89f2f8eeb8c
Amandeep Bhatia <Abhatia3288@conestogac.on.ca>
2021-02-24 22:21:46 +0000

    Merged PR 63: added server side logging to server console for incoming requests
    added server side logging to server console for incoming requests

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const logger = require('morgan');
 
 const app = express();
 const auth_route = require("./routes/auth-route");
 const user_route = require("./routes/user-route");
+
+// Log requests to the console.
+app.use(logger('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
